b86c534bee9761df444c93829fa383ca011fa645
Nathan <nathansilva406@gmail.com>
2023-10-30 16:03:18 -0300

    remove componentes em app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import AppEntrega from "./Componentes/AppEntrega";
 import Cardapio from "./Componentes/Cardapio";
 import Footer from "./Componentes/Footer";
-import Header from "./Componentes/Header";
 import Navbar from "./Componentes/Navbar";
-import Slider from "./Componentes/Slider";
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Slider/>
-      <AppEntrega/>
       <Cardapio/>
       <Footer/>
     </div>
